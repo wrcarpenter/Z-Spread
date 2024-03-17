b45374fed4ec20c056b25ca301172bb700ce1163
Will Carpenter <65867547+wrcarpenter@users.noreply.github.com>
2024-03-17 19:32:09 -0400

    Add files via upload

--- zspread-charts.py
+++ zspread-charts.py
@@ -1,8 +1,8 @@
-"""
-Bootstrap Spot Rate Curve
 
-Code for parsing treasury data, bootstrapping a spot rate curve, and generating
-various charts.
+"""
+Charting
+
+Code for charts used in the 'Z-Spread' project. 
 
 Author: Will Carpenter
 
@@ -14,105 +14,19 @@ import matplotlib.pyplot as plt
 from scipy import interpolate
 from scipy.interpolate import CubicSpline
 from matplotlib.ticker import AutoMinorLocator, MultipleLocator, MaxNLocator
-
-
+# Additioanl imports for charting
+from matplotlib.colors import LightSource
+from datetime import datetime
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.dates import DateFormatter
 
 #%%
 # Read in par-yield data
 tsy  = pd.read_csv("https://raw.githubusercontent.com/wrcarpenter/Z-Spread/main/Data/daily-treasury-rates.csv", header=0)
 head = pd.read_csv("https://raw.githubusercontent.com/wrcarpenter/Z-Spread/main/Data/daily-treasury-spot-header.csv")
 
-# Define columns 
-tsy_cols = list(tsy.columns.values)
-cols = list(head.columns.values)
 
 #%%
-# Generate interpolated yields data
-ylds   = np.empty([1,62])
-months = np.array([1,2,3,5,6,12,24,36,60,84,120,240,360])
-x      = np.linspace(0,360,61)
-
-for i in range(len(tsy)):
-    
-    row = np.array(tsy.loc[i])
-    date = row[0:1]    
-    rates = row[1:]
-    
-    f = CubicSpline(months, rates)
-    interp = f(x)
-    
-    add  = np.append(date, interp)
-    ylds = np.vstack((ylds, add))
-    
-ylds = pd.DataFrame(np.delete(ylds, 0, 0), columns=cols)   # completed yields array
-
-#%%
-
-# rows, cols 
-spots = pd.DataFrame(np.zeros((ylds.shape[0], ylds.shape[1]), dtype=float), columns=cols)
- 
-# No interpolation required here - shorter-term treasuries are ZCBs
-spots['Date'] = tsy['Date']
-spots['0']    = tsy['1']
-spots['6']    = tsy['6']
-spots['12']   = tsy['12']
-
-# Bond assumptions for bootstrap; do not modify
-face   = 100
-delta  = 1/2 
-
-# Bootstrap methodology 
-for row in range(0,spots.shape[0]):  
-    for col in range(0, spots.shape[1]):
-        
-        if col <= 3: continue # spot rates already defined 
-        
-        # Now solving for zero-coupon bond yield
-        int_cf = 0 
-        cpn    = ylds.iloc[row, col]  # interpolated coupon for par bond
-        
-        for i in range(2, col): # solve for intermediate cash flows
-                    
-            zcb    = 1/((1+spots.iloc[row, i]/100*delta)**(i-1))
-            int_cf = int_cf +cpn/100*delta*face*zcb
-                
-        zero = ((face + face*cpn/100*delta)/(face - int_cf)) # algebra to solve for zero rate
-        zero = zero**(1/(col-1))
-        zero = (zero-1)*2
-        
-        spots.iloc[row, col] = zero*100
-
-#%%
-# update/replace file 
-spots.to_csv('C:/Users/wcarp/OneDrive/Desktop/Z-Spread/Data')
-
-#%% 
-
-# Converting semi-annual spots to monthly using spline interpolation
-spots_monthly = np.empty([1,361])
-months        = np.array(spots.columns.to_list())
-months        = months[1:]
-x             = np.linspace(1,360,360)
-
-for i in range(len(spots)):
-    
-    row   = np.array(spots.loc[i])
-    date  = row[0:1]
-    rates = row[1:]
-    
-    f = CubicSpline(months, rates)
-    interp = f(x)
-    
-    add = np.append(date, interp)
-    spots_monthly = np.vstack((spots_monthly, add))
-
-spots_monthly = pd.DataFrame(np.delete(ylds, 0, 0))        
-
-# spots_monthly.to_clipboard()
-#%% Plotting (creating custom project plots)
-
-# Plot treasury points
-
 from matplotlib.colors import LightSource
 from datetime import datetime
 from mpl_toolkits.mplot3d import Axes3D
@@ -262,6 +176,7 @@ def tsy_rate_surface(elevation, azimuthal):
     
     ax.view_init(elev=elevation, azim=azimuthal)    #40,50    40,110   to flip around
     
+    
     ax.set_xlabel('Date', labelpad=20)
     ax.set_ylabel('Tenor (months)', labelpad=20)
     ax.set_zlabel('Rate (%)', labelpad=3)
@@ -281,9 +196,4 @@ plot3 = spot_rate_curve()
 #%%
 plot4 = tsy_rate_surface(40, 50)
 plot5 = tsy_rate_surface(40, 110)
-
-
-
-
-
 
